0c777f2f76e3e124cf2fa8231a567307d0686776
chernandez7 <chernandez7@luc.edu>
2017-05-05 18:31:38 -0500

    Removed Swipe Cards

--- src/Screens/CardScene.js
+++ src/Screens/CardScene.js
@@ -19,77 +19,24 @@ import {
   N1
 } from '../Kanji';
 
-import SwipeCards from 'react-native-swipe-cards';
 const window = Dimensions.get('window');
-
-let Card = React.createClass({
-  render() {
-    return (
-      <View style={styles.cardContainer}>
-        <Text style={styles.cardText}>{this.props.kanji}</Text>
-        <Text style={styles.cardText}>{this.props.onyomi}</Text>
-        <Text style={styles.cardText}>{this.props.kunyomi}</Text>
-        <Text style={styles.cardText}>{this.props.meaning}</Text>
-        <Image style={styles.cardImage} source={Images[this.props.img]}/>
-      </View>
-    )
-  }
-})
 
 export default class CardScene extends Component {
   static navigationOptions = {
     header: null,
   }
 
-  handleYup (card) {
-    console.log(`Yup for ${card.kanji}`)
-  }
-
-  handleNope (card) {
-    console.log(`Nope for ${card.kanji}`)
-  }
-
-  handleMaybe (card) {
-    console.log(`Maybe for ${card.kanji}`)
-  }
-
   render() {
     const { params } = this.props.navigation.state;
 
-        // If you want a stack of cards instead of one-per-one view, activate stack mode
-        // stack={true}
         return (
-          <View style={styles.container}>
-          <SwipeCards
-            cards={
-              (
-              (params.level == 5)? N5 :
-              (params.level == 4)? N4 :
-              (params.level == 3)? N3 :
-              (params.level == 2)? N2 :
-              N1 )
-          }
-
-            renderCard={(cardData) => <Card {...cardData} />}
-            renderNoMoreCards={() => <NoMoreCards />}
-
-            handleYup={this.handleYup}
-            handleNope={this.handleNope}
-            handleMaybe={this.handleMaybe}
-            hasMaybeAction
-            loop={true}
-          />
-          </View>
-    );
-  }
-}
-
-class NoMoreCards extends Component {
-  render() {
-    return(
-      <View style={styles.noMoreCardsContainer}>
-      <Text>No More Cards!</Text>
-      </View>
+        <View style={styles.container}>
+          <Text style={styles.cardText}>{N5[0].kanji}</Text>
+          <Text style={styles.cardText}>{N5[0].onyomi}</Text>
+          <Text style={styles.cardText}>{N5[0].kunyomi}</Text>
+          <Text style={styles.cardText}>{N5[0].meaning}</Text>
+          <Image style={styles.cardImage} source={Images[N5[0].img]}/>
+        </View>
     );
   }
 }
@@ -116,7 +63,4 @@ const styles = StyleSheet.create({
    height: null,
    resizeMode: 'cover',
   },
-  noMoreCardsContainer: {
-
-  }
 });
