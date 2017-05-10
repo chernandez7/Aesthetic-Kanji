cc799f03975c1469d148ddb8735eab04880df718
chernandez7 <chernandez7@luc.edu>
2017-05-10 02:15:44 -0500

    Fixed utf-8 parsing and populated DB

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-## Flashcard App Concept ##
+## Flashcard App ##
+React Native application to help in studying JLPT kanji using flashcards. Currently uses FireBase to manage the database.
 
-WIP
+## Example ##
+![alt-text](http://i.imgur.com/VRJzVv0.png)
+
+![alt-text](http://i.imgur.com/e6NngWN.png)
 
 # TODO #
  - ~~Set Up DB~~
  - ~~Connect to App~~
  - ~~App Flow~~
+- ~~Populate DB~~
  - Design
  - Metrics / Visuals
+ - Possible user accounts
+ - Filters / Randomization
+ - Machine Learning?
--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,4 +1,5 @@
 import sys
+import codecs
 import time
 from queue import Queue
 import requests
@@ -6,44 +7,43 @@ from bs4 import BeautifulSoup
 
 start = time.time()
 item_count = 0
-url = "https://nihongoichiban.com/2011/04/10/complete-list-of-kanji-for-jlpt-n5/"
+url = "https://nihongoichiban.com/2014/07/22/complete-list-of-kanji-for-jlpt-n3/"
 
-file = open("N5.json", "w+")
-file.write("{\n\t\"Kanji\": {\n\t\"N5\": [\n")
-print('Processing "{}"...\n'.format(url))
+def write_to_file(input):
+    """Loop to write each row in table"""
+    file.write("{0}\n".format('{'))
+    file.write("\"kanji\": \"{0}\",\n\"onyomi\": \"{1}\",\n\"kunyomi\": \"{2}\",\n\"meaning\": \"{3}\",".format(*(str(i) for i in input)))
+    file.write("\n\"img\": \"{0}\"".format('one'))
+    file.write("\n{0}".format('},'))
 
-response = requests.get(url)
-html = response.content
-soup = BeautifulSoup(html, "html.parser")
-table = soup.find("table")
+with codecs.open("N3.json", "w", "utf-8") as file:
+    file.write("{\n\"Kanji\": {\n\"N3\": [\n")
+    print('Processing "{}"...\n'.format(url))
 
-# Loop over table
-list_of_rows = []
-for row in table.findAll("tr")[1:]:
-    item_count += 1
-    list_of_cells = []
-    for cell in row.findAll("td")[1:]:
-        #for a in cell.findAll("td"):
-            #list_of_cells.append(name)
-        list_of_cells.append(cell.text)
+    response = requests.get(url)
+    html = response.content
+    soup = BeautifulSoup(html)
 
-for a in list_of_cells:
-    file.write(a)
-    """{ Example
-      "kanji": "四",
-      "onyomi": "shi",
-      "kunyomi": "yo(ttsu), yu(tsu), yo-, yon-",
-      "meaning": "four",
-      "img": "one"
-    },"""
+    # http://stackoverflow.com/questions/36833357/python-correct-encoding-of-website-beautiful-soup
+    encoding = response.encoding if 'charset' in response.headers.get('content-type', '').lower() else None
+    soup = BeautifulSoup(html, from_encoding=encoding)
 
-    #file.write("\{\n"kanji": {},\n"onyomi": {},\n"kunyomi": {},\n"meaning": {},\n"img": {},\n\},".format(input))
+    table = soup.find("table")
 
-del list_of_rows[:]
-del list_of_cells[:]
+    # Loop over table
+    list_of_rows = []
+    for row in table.findAll("tr")[1:]:
+        list_of_cells = []
+        for cell in row.findAll("td")[1:]:
+            list_of_cells.append(cell.text)
 
-file.write("]\n}\n}")
-file.close()
+        item_count += 1
+        write_to_file(list_of_cells)
+        del list_of_rows[:]
+        del list_of_cells[:]
+
+    file.write("]\n}\n}")
+    file.close()
 
 end = time.time()
 print ("Finished processing {} items in {} seconds".format(str(item_count), str(end - start)))
