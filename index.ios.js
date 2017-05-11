59e9dece144c25b8b3016a3351af2d1c596bf4bf
chernandez7 <chernandez7@luc.edu>
2017-05-10 23:17:22 -0500

    removed space in app registry

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,4 @@
 import {AppRegistry} from 'react-native';
 import Root from './src/Root';
 
-AppRegistry.registerComponent('Aesthetic Kanji', () => Root);
+AppRegistry.registerComponent('AestheticKanji', () => Root);
